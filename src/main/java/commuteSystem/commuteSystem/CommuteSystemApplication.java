2c4ddb0e39855eb8981b6c99c9ee90cf891604a9
Sondongyeon <ndn153@nextgrid.co.kr>
2024-07-03 16:22:36 +0900

    First Commit

---
+++ src/main/java/commuteSystem/commuteSystem/CommuteSystemApplication.java
@@ -0,0 +1,13 @@
+package commuteSystem.commuteSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommuteSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommuteSystemApplication.class, args);
+	}
+
+}
